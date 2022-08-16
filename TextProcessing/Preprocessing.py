9c3cb6caf6de7c29ab08d72573520600ffd5a094
RudiYare <rezan20031@gmail.com>
2022-08-16 20:57:14 +0300

    Изменена реализация подготовку текста

--- TextProcessing/Preprocessing.py
+++ TextProcessing/Preprocessing.py
@@ -1,34 +1,7 @@
 import re
 import string
-from pymorphy2 import MorphAnalyzer
-from nltk.tokenize import word_tokenize
+
 from nltk.corpus import stopwords
-
-from nltk.stem.porter import PorterStemmer
-morph = MorphAnalyzer()
-stemmer = PorterStemmer()
-
-
-def set_of_tokens(text: str, lang: str = 'english'):
-    """
-    НЕ ИСПЫТАНО!
-    """
-    text = preprocessing(text)
-    text_tokens = word_tokenize(text)
-    text_tokens = remove_stopwords(text_tokens, lang)
-
-    normal_tokens = [morph.normal_forms(token) for token in text_tokens]
-    print(normal_tokens)
-
-
-    normal_tokens = [stemmer.stem(word) for word in text_tokens]
-    print(normal_tokens)
-    text_tokens = set([morph.normal_forms(token) for token in text_tokens])
-    return None
-
-
-def dict_of_tokens(text: str, lang: str = 'english'):
-    pass
 
 
 def preprocessing(text: str):
--- TextProcessing/__init__.py
+++ TextProcessing/__init__.py
@@ -2,9 +2,10 @@ import inflect
 import nltk
 import string
 import re
+import numpy
 
-from Preprocessing import set_of_tokens
+from Processing import create_tree_entities
 
 if __name__ == '__main__':
     text = 'Птицекрылка королевы Александры, или птицекрылка Александры, или птицекрыл королевы Александры, или орнитоптера Александра, или орнитоптера королевы Александры (Ornithoptera alexandrae) — вид дневных бабочек из рода Ornithoptera семейства парусников. Размах крыльев самцов достигает 14,7—22 см, самок — 18,7—24,8 см, а по некоторым данным даже до 28—30 см. Считается одной из крупнейших по размаху крыльев дневных бабочек в мире.'
-    print(set_of_tokens(text, 'russian'))
+    print(create_tree_entities(text))
